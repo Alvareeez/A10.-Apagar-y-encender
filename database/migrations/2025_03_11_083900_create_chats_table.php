d03c3e3d408c26e0b935a74f21ec77a0881e0f4e
Adrian Vazquez <100007005.joan23@fje.edu>
2025-03-12 18:54:42 +0100

    Cambios

--- database/migrations/2025_03_11_083900_create_chats_table.php
+++ database/migrations/2025_03_11_083900_create_chats_table.php
@@ -1,4 +1,5 @@
 <?php
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -12,9 +13,9 @@ class CreateChatsTable extends Migration
     {
         Schema::create('chats', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('incidencia_id')->constrained('incidencias')->onDelete('cascade');
-            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
-            $table->foreignId('tecnico_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('incidencia_id')->constrained('incidencia')->onDelete('cascade');
+            $table->foreignId('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreignId('tecnico_id')->references('id')->on('users')->onDelete('cascade');
             $table->text('message');
             $table->timestamps();
         });
